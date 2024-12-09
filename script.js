b5280d0df19d3f6b12246f102b7b1ad941776ccd
Cakeytoo <88193527+Cakeytoo@users.noreply.github.com>
2024-12-09 16:56:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://924637b1-4ab3-4b45-8860-02352b7f28d0-00-8r45cfl342qf.sisko.replit.dev')
+fetch('https://924637b1-4ab3-4b45-8860-02352b7f28d0-00-8r45cfl342qf.sisko.replit.dev:8080/')
     .then(response => {
         console.log("Response:", response);
         return response.text();
