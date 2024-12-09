5fbe03b95ab1891d7c38f38bd4bab10e3c1e384f
Cakeytoo <88193527+Cakeytoo@users.noreply.github.com>
2024-12-09 16:36:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch('https://924637b1-4ab3-4b45-8860-02352b7f28d0-00-8r45cfl342qf.sisko.replit.dev')
-    .then(response => response.text())
+ .then(response => response.text())
     .then(data => {
         document.getElementById('output').innerText = data;
     })
